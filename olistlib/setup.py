2f471510cd83b2983f0619265f42adf937f4bffe
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2020-06-11 12:20:28 -0300

    parte 3 02:59 - criado o pacote olistlib, a partir de agora vamos mudar o BD de sqlite para MariaDB

---
+++ olistlib/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name='olistlib',
+    version='0.1',
+    description='Biblioteca para suporte de módulos nos projetos da empresa Olist',
+    author='Marcelo Shirayama',
+    author_email='caixadecorreiodomarcelo@gmail.com',
+    packages=['olistlib']
+)
